bbc147ea5e201e2e5b83b1b1d60d24912c10e3a8
chloeehaynes <cehaynes@wustl.edu>
2022-07-14 10:26:32 -0500

    added home page

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Where should we eat?">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="style.css" type="text/css" rel="stylesheet">
+
+    <title>Where the F?</title>
+</head>
+
+
+<body>
+<heading>
+    <h1>Where the F?</h1>
+    <h2>(F as in food of course...)</h2>
+</heading>
+
+<section>
+    <button>return to group</button>
+    <button>start a group</button>
+</section>
+
+
+
+</body>
+
+</html>
